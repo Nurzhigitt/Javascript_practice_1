3777c51fd1f757d48a168d120c31195f244be163
Nurzhigit <Nurzhigit00a@gmail.com>
2022-11-05 17:28:05 +0500

    First-commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = prompt("Do you have 18 years?", "18");
+// console.log(typeof(answer));
+// const answers = [];
+// answers[0] = prompt('Kak vasha imya?', '');
+// answers[1] = prompt('Kak vasha familia?', '');
+// answers[2] = prompt('Skolko vam let?', '');
+// document.write(answers);
+
+
+
+// const category = 'toys';
+// const gregory = 'gloves';
+// console.log(`https://someurl.com/${category}/5`);
+// console.log(`https://boxer.com/${gregory}/shop_page`);
+
+
+// const user1 = 'Erzhan';
+// alert(`Hi, ${user1}`);
+
+// console.log('arr' + ' - object');
+// console.log(4 + "5");
+// console.log(4 + +"3");
+
+let incr = 10;
+let decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(--incr);
+// console.log(++decr);
+
+// console.log(5%2);
+
+// console.log(2 * 4 === 8);
+
+const isChecked = true;
+const isClose = false;
+
+console.log(isChecked || !isClose);
